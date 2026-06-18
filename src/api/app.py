a9d968205bcb3591a475cd44fa8e3e2245e61e17
TIEUPHUONG1711 <tp17112k4@gmail.com>
2026-06-18 16:50:35 +0700

    api v2

--- src/api/app.py
+++ src/api/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 PrometheusMetrics(app)  # Tự thêm /metrics
 
 ERROR_RATE = float(os.getenv("ERROR_RATE", "0"))
-VERSION = os.getenv("VERSION", "v1")
+VERSION = os.getenv("VERSION", "v2")
 
 @app.get("/")
 def index():
@@ -16,9 +16,11 @@ def index():
         return jsonify(error="injected", version=VERSION), 500
     return jsonify(ok=True, version=VERSION)
 
+
 @app.get("/healthz")
 def healthz():
     return "ok", 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
